4cd132e90863a0eeb1f78e7a832641b71877c23f
Pekka Enberg <penberg@kernel.org>
2012-09-03 18:43:10 +0300

    vax: IRQ
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ arch/vax/include/asm/irq.h
@@ -0,0 +1,9 @@
+#ifndef _VAX_IRQ_H
+#define _VAX_IRQ_H
+
+#define NR_IRQS		384
+#include <asm-generic/irq.h>
+
+#define NO_IRQ		0
+
+#endif
---
+++ arch/vax/include/asm/irqflags.h
@@ -0,0 +1,9 @@
+#ifndef _VAX_IRQFLAGS_H
+#define _VAX_IRQFLAGS_H
+
+#define ARCH_IRQ_DISABLED        0x00
+#define ARCH_IRQ_ENABLED         0x01 /* FIXME */
+
+#include <asm-generic/irqflags.h>
+
+#endif
