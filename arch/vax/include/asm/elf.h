a70ef69a0ea60ee8a50773a2254dfe20a4876e6f
Pekka Enberg <penberg@kernel.org>
2012-09-03 19:35:41 +0300

    vax: ELF definitions
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ arch/vax/include/asm/elf.h
@@ -0,0 +1,8 @@
+#ifndef _VAX_ELF_H
+#define _VAX_ELF_H
+
+#define ELF_CLASS	ELFCLASS32
+#define ELF_DATA	ELFDATA2LSB
+#define ELF_ARCH	EM_VAX
+
+#endif
---
+++ arch/vax/include/asm/param.h
@@ -0,0 +1,8 @@
+#ifndef _VAX_PARAM_H
+#define _VAX_PARAM_H
+
+#define EXEC_PAGESIZE	4096
+
+#include <asm-generic/param.h>
+
+#endif
